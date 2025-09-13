a45e8878c11e85910bdf0ba2e6420216ea2f18c7
William Song <wjhsong@gmail.com>
2025-09-13 09:15:38 +0900

    Update whoami.c

--- user/whoami.c
+++ user/whoami.c
@@ -3,9 +3,9 @@
 #include "user/user.h"
 #include "user/sid.h"
 
-int main(int argc, char **argv) {
-    printf("Student ID:   %d\n", sid);
-    printf("Student name: %s\n", sname);
-    exit(0);
+int
+main(int argc, char **argv) {
+  printf("Student ID:   %d\n", sid);
+  printf("Student name: %s\n", sname);
+  exit(0);
 }
-
